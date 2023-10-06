1caddd7991f8962a02427bb6d61d4cd05b989ad0
Eugene Zolotarev <eugzol@gmail.com>
2023-10-07 00:29:20 +0400

    WIP

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -83,4 +83,32 @@ RSpec.describe Datacaster::Transaction do
       email: {address: 'john@example.org', result: true}
     )
   end
+
+  it "allows to define steps in a block" do
+    return pending
+    tx =
+      Class.new do
+        include Datacaster::Transaction
+
+        transform_step :unwrap
+        cast_step typecast
+        cast_step with(:email, send_email)
+        perform { sequence(
+          unwrap,
+          typecast,
+          with(:email, send_email)
+        ) }
+
+        steps.unwrap = transform { |x| x.to_h }
+        steps.typecast = hash_schema(name: string, email: string)
+        steps.send_email = transform do |email|
+          {address: email, result: true}
+        end
+      end
+
+    expect(tx.(name: 'John', email: 'john@example.org').to_dry_result).to eq Success(
+      name: 'John',
+      email: {address: 'john@example.org', result: true}
+    )
+  end
 end
